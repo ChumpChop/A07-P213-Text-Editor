ca8cb16d203425d2010daa91ea3f7f20becc8e66
Clive B Harrison <clive_b_harrison@outlook.com>
2021-10-28 21:49:26 +0100

    Add project files.

---
+++ WPF Text Editor/TextEditor.xaml.vb
@@ -0,0 +1,115 @@
+﻿Class TextEditor
+
+    'Get or set the text on the status bar
+    Public Property StatusText() As String
+        Get
+            Return sbiStatus.Content.ToString
+        End Get
+        Set(ByVal value As String)
+            sbiStatus.Content = value
+        End Set
+    End Property
+
+    'Gets or sets the text that you’re editing
+    Public Property EditText() As String
+        Get
+            Return txtEdit.Text
+        End Get
+        Set(ByVal value As String)
+            txtEdit.Text = value
+        End Set
+    End Property
+
+    'Clears the txtEdit control
+    Public Sub ClearEditBox()
+        'Set the EditText property
+        EditText = String.Empty
+
+        'Reset the font color
+        txtEdit.Foreground = Brushes.Black
+
+        'Set the status bar text
+        StatusText = "Text box cleared"
+    End Sub
+
+    Private Sub txtEdit_TextChanged(ByVal sender As Object, _
+        ByVal e As System.Windows.Controls.TextChangedEventArgs) _
+        Handles txtEdit.TextChanged
+
+        'Reset the status bar text
+        StatusText = "Ready"
+    End Sub
+
+    Private Sub tbrClear_Click(ByVal sender As Object, _
+    ByVal e As System.Windows.RoutedEventArgs) _
+    Handles tbrClear.Click
+
+        'Clear the edit box
+        ClearEditBox()
+    End Sub
+
+    Public Sub RedText()
+        'Make the text red
+        txtEdit.Foreground = Brushes.Red
+
+        'Update the status bar text
+        StatusText = "The text is red"
+    End Sub
+
+    Private Sub tbrRed_Click(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles tbrRed.Click
+
+        'Make the text red
+        RedText()
+    End Sub
+
+    Public Sub BlueText()
+        'Make the text blue
+        txtEdit.Foreground = Brushes.Blue
+
+        'Update the status bar text
+        StatusText = "The text is blue"
+    End Sub
+
+    Private Sub tbrBlue_Click(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles tbrBlue.Click
+
+        'Make the text blue
+        BlueText()
+    End Sub
+
+    Public Sub UpperCaseText()
+        'Make the text uppercase
+        EditText = EditText.ToUpper
+
+        'Update the status bar text
+        StatusText = "The text is all uppercase"
+    End Sub
+
+    Private Sub tbrUpperCase_Click(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles tbrUpperCase.Click
+
+        'Make the text uppercase
+        UpperCaseText()
+    End Sub
+
+    Public Sub LowerCaseText()
+        'Make the text lowercase
+        EditText = EditText.ToLower
+
+        'Update the status bar text
+        StatusText = "The text is all lowercase"
+    End Sub
+
+    Private Sub tbrLowerCase_Click(ByVal sender As Object, _
+        ByVal e As System.Windows.RoutedEventArgs) _
+        Handles tbrLowerCase.Click
+
+        'Make the text lowercase
+        LowerCaseText()
+    End Sub
+
+End Class
---
+++ Windows Forms Text Editor/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Windows Forms Text Editor")> 
+<Assembly: AssemblyDescription("Provides an introduction to complex programming.")> 
+<Assembly: AssemblyCompany("John Wiley && Sons, Inc.")> 
+<Assembly: AssemblyProduct("Windows Forms Text Editor")> 
+<Assembly: AssemblyCopyright("Copyright ©  2009 John Wiley && Sons, Inc.")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("53b3089d-0cc0-47ca-9ff0-ce22c0cbe224")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
